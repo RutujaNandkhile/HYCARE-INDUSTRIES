8a65210bba86b7b8db6125bab506b45e354987fd
Rutuja Nandkhile <rutujanandkhile11@gmail.com>
2026-08-19 18:40:34 +0530

    login issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("MongoDB connection error:", error.message);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
